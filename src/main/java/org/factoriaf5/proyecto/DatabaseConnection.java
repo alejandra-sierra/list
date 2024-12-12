efb0bd1fca086c2a3e2831b8ec40a0f1e40fa0c2
Alejandra <siemi121001@gmail.com>
2024-12-13 00:50:47 +0100

    fix database

--- src/main/java/org/factoriaf5/proyecto/DatabaseConnection.java
+++ src/main/java/org/factoriaf5/proyecto/DatabaseConnection.java
@@ -1,9 +1,21 @@
 package org.factoriaf5.proyecto;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/todolist";
     private static final String USER = "root";
     private static final String PASSWORD = "1234";
 
-
+    public static void main(String[] args) {
+        try {
+            @SuppressWarnings("unused")
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to database!");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to database: " + e.getMessage());
+        }
+    }
 }

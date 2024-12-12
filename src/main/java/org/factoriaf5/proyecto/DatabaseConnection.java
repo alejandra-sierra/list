33e47ab82f7681e27a70ae46836ffdf0f0eb3c4c
Alejandra <siemi121001@gmail.com>
2024-12-13 00:15:48 +0100

    database connection

---
+++ src/main/java/org/factoriaf5/proyecto/DatabaseConnection.java
@@ -0,0 +1,9 @@
+package org.factoriaf5.proyecto;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/todolist";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+
+
+}

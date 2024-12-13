2544bfb32ca786373847922fddf206ab1acb022f
Alejandra <siemi121001@gmail.com>
2024-12-13 02:09:27 +0100

    changes controller

--- src/main/java/org/factoriaf5/proyecto/controllers/TaskController.java
+++ src/main/java/org/factoriaf5/proyecto/controllers/TaskController.java
@@ -6,15 +6,15 @@ import org.factoriaf5.proyecto.models.Task;
 import org.factoriaf5.proyecto.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController  
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
 
@@ -42,12 +42,12 @@ public class TaskController {
     }
 
     @PutMapping("/{id}") 
-    public ResponseEntity<Task> updateTask(Task task) { 
+    public ResponseEntity<Task> updateTask(@RequestBody Task task) { 
         return ResponseEntity.ok(taskService.updateTask(task)); 
     } 
 
     @DeleteMapping("/{id}") 
-    public ResponseEntity<Boolean> deleteTask(Task task) { 
+    public ResponseEntity<Boolean> deleteTask(@RequestBody Task task) { 
         taskService.deleteTask(task); 
         return ResponseEntity.ok(true); 
     }
